e47184168f1b09e62a7f6def8d3f7eabdebcf7e7
geekprit <55937327+geekprit@users.noreply.github.com>
2021-03-28 10:32:57 +0530

    Update list.dart

--- newapp/lib/list.dart
+++ newapp/lib/list.dart
@@ -1,48 +1,24 @@
 import 'package:flutter/material.dart';
-import 'package:newapp/list.dart';
 
-void main() => runApp(MaterialApp(
-      title: 'MyApp',
-      // implementing named routes in our material app
-      // home: MyApp(), The home parameter is now no longer needed and now initialroute parameter is used instead
-      initialRoute: '/home', // the /home is defined in `routes`
-      // The Routes takes in a list shown below
-      routes: {
-        "/home": (context) => MyApp(),
-        "/second": (context) => SecondPage(),
-      },
-      theme: ThemeData(primaryColor: Colors.amber),
-    ));
-
-class MyApp extends StatelessWidget {
+class SecondPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(primarySwatch: Colors.teal),
-      title: 'Material App',
-      home: SafeArea(
-        child: Scaffold(
-            // removed appbar and wrapped scaffold with safeArea
-            body: GridView.builder(
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                ),
-                itemBuilder: (BuildContext context, int index) {
-                  return ElevatedButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => SecondPage(),
-                        ),
-                      );
-                    },
-                    child: Hero(tag: 'icon_hero',
-                    transitionOnUserGestures: true,
-                      child: Icon(Icons.access_alarm,
-                      size:75 ,))
-                  );
-                })),
+    return Scaffold(
+      body: Container(
+        child: ListView.builder(
+          itemCount: 200,
+          itemBuilder: (BuildContext context, int index) {
+            return ListTile(
+              leading: Hero(
+                tag:'icon_hero',
+                transitionOnUserGestures: true,
+                
+                child: Icon(Icons.access_alarm)),
+              title: Text('one'),
+              onTap: () => Navigator.pushNamed(context, '/'),
+            );
+          },
+        ),
       ),
     );
   }
